24747b16ca5eaa4bdf4222f24fb08d0b465a408b
jamabdel <82024123+jamabdel@users.noreply.github.com>
2021-09-15 12:14:08 +0200

    bodyParser

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,19 @@
-const express = require('express');
-
+const express = require('express'); //inpoirtation de espress
+const bodyParser = require('body-parser')
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(function (req, res, next) {
+    console.log('requête reçue!');
+    next();
+
+});
 
 app.use(function (req, res, next) {
     res.json({
